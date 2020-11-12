1fe7116c64b4fa29d57b3036ed70e99565bcd48e
Ahmet Bozaci <ahmt9417@gmail.com>
2020-11-12 18:55:24 +0300

    fixed rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
 
 class Interface
-
   @@arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   def create_table
-    @@table = <<EOM
+    @table = "
      =------------------------=
      |   #{@@arr[0]}   |   #{@@arr[1]}    |   #{@@arr[2]}   |
      --------------------------
@@ -13,66 +12,54 @@ class Interface
      --------------------------
      |   #{@@arr[6]}   |   #{@@arr[7]}    |   #{@@arr[8]}   |
      =------------------------=
-EOM
+"
   end
 
   def display
     puts create_table
     puts ''
-    puts 'Player one is "O" '
+    puts 'Player ONE is "X" '
     puts ''
-    puts 'Player two is "X" '
+    puts 'Player TWO is "O" '
     puts ''
   end
 
-  def get_number
-        while @@arr.any? { |element| element.is_a?Integer }
+  def number
+    while @@arr.any?(Integer)
 
-        puts 'Player one enter a number:'
-        player_one_input = gets.chomp
-        update_table(player_one_input.to_i)
-        change_player
-        puts ''
-        puts 'Player two enter a number:'
-        player_two_input = gets.chomp
-        update_table(player_two_input.to_i)
-        change_player
-        end
-        puts 'Game finished!'
+      puts 'Player ONE enter a number:'
+      player_one_input = gets.chomp
+      update_table(player_one_input.to_i)
+      change_player
+      puts ''
+      puts 'Player TWO enter a number:'
+      player_two_input = gets.chomp
+      update_table(player_two_input.to_i)
+      change_player
+    end
+    puts 'Game finished!'
   end
 
-  @@active_player = 'player_one'
+  @active_player = 'player_one'
   def change_player
-    @@active_player = if @@active_player == 'player_one'
-                        'player_two'
-                      else
-                        'player_one'
-                      end
+    @active_player = if @active_player == 'player_one'
+                       'player_two'
+                     else
+                       'player_one'
+                     end
   end
 
   def update_table(num)
-    if @@active_player == 'player_one'
-
-      puts "passed number is #{num}"
-      puts "array item #{@@arr[num - 1]} will be replaced with O]"
-      puts "active player is #{@@active_player}"
-      @@arr[num - 1] = 'O'
-      puts @@arr
-    else
-      puts "array item #{@@arr[num - 1]} will be replaced with X]"
-      puts "passed number is #{num}"
-      puts "active player is #{@@active_player}"
-      @@arr[num - 1] = 'X'
-      puts @@arr
-    end
+    @@arr[num - 1] = if @active_player == 'player_one'
+                       'O'
+                     else
+                       'X'
+                     end
     puts create_table
   end
-
-
 end
-
 
 game = Interface.new
 game.display
 game.change_player
-game.get_number
+game.number
