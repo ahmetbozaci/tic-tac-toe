f4aca6324ceee6a8dcdc5f4bd4fab293f79f6991
Rayan <rayan_84@live.com>
2020-11-12 14:02:59 +0100

    Add create table method Fix player Add debugging code to methods Call change_player method from get_number method

--- bin/main.rb
+++ bin/main.rb
@@ -3,38 +3,41 @@
 class Interface
 
   @@arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  @@table = <<EOM
-   =------------------------=
-   |   #{@@arr[0]}   |   #{@@arr[1]}    |   #{@@arr[2]}   |
-   --------------------------
-   |   #{@@arr[3]}   |   #{@@arr[4]}    |   #{@@arr[5]}   |
-   --------------------------
-   |   #{@@arr[6]}   |   #{@@arr[7]}    |   #{@@arr[8]}   |
-   =------------------------=
-EOM
 
-  
+  def create_table
+    @@table = <<EOM
+     =------------------------=
+     |   #{@@arr[0]}   |   #{@@arr[1]}    |   #{@@arr[2]}   |
+     --------------------------
+     |   #{@@arr[3]}   |   #{@@arr[4]}    |   #{@@arr[5]}   |
+     --------------------------
+     |   #{@@arr[6]}   |   #{@@arr[7]}    |   #{@@arr[8]}   |
+     =------------------------=
+EOM
+end
+
   def display
-    puts @@table
+    puts create_table
     puts ""
     puts 'Player one is "O" '
     puts ""
     puts 'Player two is "X" '
     puts ""
   end
- 
+
   def get_number
+
     puts 'Player one enter a number:'
-    player_one_input = gets.chomp.to_i
-    update_table(player_one_input)
-    puts @@table
+    player_one_input = gets.chomp
+    update_table(player_one_input.to_i)
+    change_player
     puts ""
     puts 'Player two enter a number:'
-    player_two_input = gets.chomp.to_i
-    update_table(player_two_input)
-    puts @@table
+    player_two_input = gets.chomp
+    update_table(player_two_input.to_i)
+    change_player
   end
-  
+
   @@active_player = "player_one"
   def change_player
     if @@active_player == "player_one"
@@ -45,23 +48,31 @@ EOM
   end
 
   def update_table(num)
-   
+
       if @@active_player == "player_one"
+
+        puts "passed number is #{num}"
+        puts "array item #{@@arr[num-1]} will be replaced with O]"
+        puts "active player is #{@@active_player}"
         @@arr[num-1] = "O"
+        puts @@arr
       else
+        puts "array item #{@@arr[num-1]} will be replaced with X]"
+        puts "passed number is #{num}"
+        puts "active player is #{@@active_player}"
         @@arr[num-1] = "X"
+        puts @@arr
       end
+      puts create_table
   end
 
 
 
 
 end
-  
+
   game = Interface.new
 
   game.display
   game.change_player
   game.get_number
-  game.display
-
