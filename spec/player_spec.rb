fb89389327cfc58f7a30f9c2245854c1c7b66b40
Ahmet Bozaci <ahmt9417@gmail.com>
2020-12-17 10:44:10 +0300

    fixed player mark

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,11 @@
 describe Player do
-  let(:player) { Player.new('name', 'mark') }
+  let(:player) { Player.new('Player1', 'X') }
 
   it ' checks for player names' do
-    expect(player.name).to eq('name')
+    expect(player.name).to eq('Player1')
   end
 
   it ' checks for player mark' do
-    expect(player.mark).to eq('mark')
+    expect(player.mark).to eq('X')
   end
 end
