130a266ed3e7b5689d141f5c23ab977e6114f2c9
Ahmet Bozaci <ahmt9417@gmail.com>
2020-12-17 11:25:19 +0300

    deleted player test in game.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,14 +18,3 @@ describe Game do
   end
 end
 
-describe Player do
-  let(:player) { Player.new('name', 'mark') }
-
-  it ' checks for player names' do
-    expect(player.name).to eq('name')
-  end
-
-  it ' checks for player mark' do
-    expect(player.mark).to eq('mark')
-  end
-end
